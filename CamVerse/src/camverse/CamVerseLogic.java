90a833b73ce6d8ff1c129f4c7b12f1f5251dfbcc
José Ángel Pastrana Padilla <japp0005@red.ujaen.es>
2015-05-28 15:05:37 +0200

    Clearing before starting.

--- CamVerse/src/camverse/CamVerseLogic.java
+++ CamVerse/src/camverse/CamVerseLogic.java
@@ -31,20 +31,30 @@ public class CamVerseLogic {
         return (l.isEmpty()) ? null : (active!=null) ? active : Webcam.getDefault();
     }
     
+    /**
+     * <b>Dada una imagen y un ángulo de rotación en grados, gira la imagen respecto al ángulo.</b>
+     * @param img Imagen de entrada.
+     * @param rotation Ángulo de rotación.
+     * @return Imagen de salida.
+     */
     public static BufferedImage rotateBI(BufferedImage img, int rotation) {
         int w = img.getWidth();  
         int h = img.getHeight();  
-        BufferedImage newImage = new BufferedImage(640, 480, BufferedImage.TYPE_INT_RGB);
+        BufferedImage newImage = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
         Graphics2D g2 = newImage.createGraphics();
         g2.rotate(Math.toRadians(rotation), w/2, h/2);  
         g2.drawImage(img,null,0,0);
         return newImage;  
     }
     
+    /**
+     * <b>Dada una imagen y un factor de escala, aumenta o disminuye la imagen (ocupando las mismas dimensiones).</b>
+     * @param img Imagen de entrada.
+     * @param ss Factor de escala.
+     * @return Imagen de salida.
+     */
     public static BufferedImage zoomBI(BufferedImage img, float ss) {
-        int w = img.getWidth();  
-        int h = img.getHeight();  
-        BufferedImage newImage = new BufferedImage(640, 480, BufferedImage.TYPE_INT_RGB);
+        BufferedImage newImage = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_INT_RGB);
         Graphics2D g2 = newImage.createGraphics();
         g2.scale(ss,ss);  
         g2.drawImage(img,null,0,0);
