fe53795d8eccc93658629122173a569ad0e1dbb9
José Ángel Pastrana Padilla <japp0005@red.ujaen.es>
2015-05-29 16:12:04 +0200

    Dialog modal for QR implemented.

--- CamVerse/src/camverse/QR.java
+++ CamVerse/src/camverse/QR.java
@@ -13,7 +13,17 @@ import com.google.zxing.NotFoundException;
 import com.google.zxing.Result;
 import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import com.google.zxing.common.HybridBinarizer;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 /**
  * <b>Hilo demonio en segundo plano que lee código QR cada cierto tiempo.</b>
@@ -32,6 +42,9 @@ public class QR implements Runnable {
         instance = _instance;
     }
     
+    /**
+     * <b>Traza de ejecución cada 100 milisegundos.</b>
+     */
     @Override
     public void run() {
         while (true) {
@@ -44,26 +57,50 @@ public class QR implements Runnable {
             Webcam webcam = instance.getWebcam();
             Result result = null;
             BufferedImage image = null;
-            
+
             if (webcam.isOpen()) {
-                    if ((image = webcam.getImage()) == null) {
-                            continue;
-                    }
+                if ((image = webcam.getImage()) == null) {
+                    continue;
+                }
 
-                    LuminanceSource source = new BufferedImageLuminanceSource(image);
-                    BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+                LuminanceSource source = new BufferedImageLuminanceSource(image);
+                BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
 
-                    try {
-                            result = new MultiFormatReader().decode(bitmap);
-                    } catch (NotFoundException e) {
-                            // fall thru, it means there is no QR code in image
-                    }
+                try {
+                    result = new MultiFormatReader().decode(bitmap);
+                } catch (NotFoundException e) {
+                        // QR no era una imagen.
+                }
             }
 
             if (result != null) {
-                    System.out.println(result.getText());
+                new AboutDialog(instance, "Código detectado", result.getText());
             }
         }
     }
-    
+    private class AboutDialog extends JDialog implements ActionListener {
+        public AboutDialog(JFrame parent, String title, String message) {
+            super(parent, title, true);
+            if (parent != null) {
+                Dimension parentSize = parent.getSize(); 
+                Point p = parent.getLocation(); 
+                setLocation(p.x + parentSize.width / 4, p.y + parentSize.height / 4);
+            }
+            JPanel messagePane = new JPanel();
+            messagePane.add(new JLabel(message));
+            getContentPane().add(messagePane);
+            JPanel buttonPane = new JPanel();
+            JButton button = new JButton("OK"); 
+            buttonPane.add(button); 
+            button.addActionListener(this);
+            getContentPane().add(buttonPane, BorderLayout.SOUTH);
+            setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+            pack(); 
+            setVisible(true);
+        }
+        public void actionPerformed(ActionEvent e) {
+          setVisible(false); 
+          dispose();
+        }
+    }
 }
